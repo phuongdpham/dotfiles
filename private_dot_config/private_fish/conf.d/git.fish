9c325d7b1053f0cfae8f363dcf03b3f91dee0e26
phuongdpham <phamduyphuong10@gmail.com>
2026-03-21 08:58:39 +0700

    Update .config/fish/conf.d/git.fish

--- private_dot_config/private_fish/conf.d/git.fish
+++ private_dot_config/private_fish/conf.d/git.fish
@@ -1,44 +1,67 @@
-alias g git
-alias ga "git add"
-alias gap "git add -p"
-alias gau "git add -u"
-alias gb "git branch"
-alias gbD "git branch -D"
-alias gbd "git branch -d"
-alias gc "git commit"
-alias gca "git commit -a"
-alias gcaa "git commit -a --amend"
-alias gcam "git commit -am"
-alias gch "git checkout"
-alias gchb "git checkout -b"
-alias gchm "git checkout main"
-alias gcm "git commit -m"
-alias gd "git diff"
-alias gdc "git diff --cached"
-alias gf "git fetch"
-alias gfo "git fetch origin"
-alias gfu "git fetch upstream"
-alias gl "git log"
-alias glG "git log -G"
-alias glo "git log --oneline"
-alias gm "git merge"
-alias gpul "git pull"
-alias gpulo "git pull origin"
-alias gpulu "git pull upstream"
-alias gpus "git push"
-alias gpusf "git push --force-with-lease"
-alias gpusu "git push -u"
-alias gpusuo "git push -u origin"
-alias gr "git rebase"
-alias gri "git rebase -i"
-alias grm "git remote"
-alias grp "git rev-parse"
-alias gs "git status"
-alias gsh "git show"
-alias gsp "git stash pop"
-alias gst "git stash"
-alias gsu "git submodule update"
-alias gsui "git submodule update --init"
-alias gsuir "git submodule update --init --recursive"
-alias gsw "git switch"
-alias gswc "git switch -c"
+# Core
+abbr -a g git
+abbr -a gs 'git status'
+
+# Add
+abbr -a ga 'git add'
+abbr -a gap 'git add -p'
+abbr -a gau 'git add -u'
+
+# Branch
+abbr -a gb 'git branch'
+abbr -a gbd 'git branch -d'
+abbr -a gbD 'git branch -D'
+
+# Checkout / Switch
+abbr -a gch 'git checkout'
+abbr -a gchb 'git checkout -b'
+abbr -a gchm 'git checkout main'
+abbr -a gsw 'git switch'
+abbr -a gswc 'git switch -c'
+
+# Commit
+abbr -a gc 'git commit'
+abbr -a gca 'git commit -a'
+abbr -a gcaa 'git commit -a --amend'
+abbr -a gcam 'git commit -am'
+abbr -a gcm 'git commit -m'
+
+# Diff
+abbr -a gd 'git diff'
+abbr -a gdc 'git diff --cached'
+
+# Fetch / Pull / Push
+abbr -a gf 'git fetch'
+abbr -a gfo 'git fetch origin'
+abbr -a gfu 'git fetch upstream'
+abbr -a gpul 'git pull'
+abbr -a gpulo 'git pull origin'
+abbr -a gpulu 'git pull upstream'
+abbr -a gpus 'git push'
+abbr -a gpusf 'git push --force-with-lease'
+abbr -a gpusu 'git push -u'
+abbr -a gpusuo 'git push -u origin'
+
+# Log
+abbr -a gl 'git log'
+abbr -a glG 'git log -G'
+abbr -a glo 'git log --oneline'
+
+# Merge / Rebase
+abbr -a gm 'git merge'
+abbr -a gr 'git rebase'
+abbr -a gri 'git rebase -i'
+
+# Stash
+abbr -a gst 'git stash'
+abbr -a gsp 'git stash pop'
+
+# Submodule
+abbr -a gsu 'git submodule update'
+abbr -a gsui 'git submodule update --init'
+abbr -a gsuir 'git submodule update --init --recursive'
+
+# Utility
+abbr -a grm 'git remote'
+abbr -a grp 'git rev-parse'
+abbr -a gsh 'git show'
