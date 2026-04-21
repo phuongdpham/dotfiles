0e58238413891902146608f0e183c31a13f80070
phuongdpham <phamduyphuong10@gmail.com>
2026-04-21 17:19:59 +0700

    Update .config/nvim/lua/core/autocmds.lua Update .config/nvim/lua/core/plugins.lua

--- private_dot_config/nvim/lua/core/autocmds.lua
+++ private_dot_config/nvim/lua/core/autocmds.lua
@@ -69,3 +69,14 @@ vim.api.nvim_create_autocmd({ 'BufAdd', 'BufDelete' }, {
         end)
     end,
 })
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+    pattern = { os.getenv("HOME") .. "/.local/share/chezmoi/*" },
+    callback = function(ev)
+        local bufnr = ev.buf
+        local edit_watch = function()
+            require("chezmoi.commands.__edit").watch(bufnr)
+        end
+        vim.schedule(edit_watch)
+    end,
+})
--- private_dot_config/nvim/lua/core/plugins.lua
+++ private_dot_config/nvim/lua/core/plugins.lua
@@ -209,6 +209,40 @@ require('blink.cmp').setup {
 
 -- Chezmoi
 require('chezmoi').setup {
+    edit = {
+        watch = false,
+        force = false,
+        ignore_patterns = {
+            "run_onchange_.*",
+            "run_once_.*",
+            "%.chezmoiignore",
+            "%.chezmoitemplate",
+            -- Add custom patterns here
+        },
+    },
+    events = {
+        on_open = {
+            notification = {
+                enable = true,
+                msg = "Opened a chezmoi-managed file",
+                opts = {},
+            },
+        },
+        on_watch = {
+            notification = {
+                enable = true,
+                msg = "This file will be automatically applied",
+                opts = {},
+            },
+        },
+        on_apply = {
+            notification = {
+                enable = true,
+                msg = "Successfully applied",
+                opts = {},
+            },
+        },
+    },
     telescope = { select = { '<CR>' } },
 }
 
